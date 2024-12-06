2e428b2b9543de382a7546a43dcc5d09406368ea
wendovsky <wendovsky@hotmail.com>
2024-12-06 05:18:14 -0800

    PcapNG

--- src/main/java/ru/wendovsky/pcapng/PcapNG.java
+++ src/main/java/ru/wendovsky/pcapng/PcapNG.java
@@ -4,11 +4,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.experimental.FieldDefaults;
-import ru.wendovsky.pcapng.block.InterfaceDescriptionBlock;
-import ru.wendovsky.pcapng.block.SectionHeaderBlock;
+import ru.wendovsky.pcapng.block.*;
 import ru.wendovsky.pcapng.exception.PcapNGFileFormatException;
 import ru.wendovsky.pcapng.reader.Reader;
-import ru.wendovsky.pcapng.block.Block;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +16,8 @@ import java.util.function.Function;
 @Accessors(fluent = true)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public final class PcapNG {
+    // Block type, length, length
+    private static final int META_BYTES_PER_BLOCK_COUNT = 12;
     public static final int ALIGNMENT = 4;
     private static final Map<Integer, Function<Reader, Block>> BLOCK_TYPE_TO_BLOCK_FACTORY = Map.of(
             0x0A0D0D0A, SectionHeaderBlock::new,
@@ -34,16 +34,22 @@ public final class PcapNG {
         List<Block> blocks = new ArrayList<>();
         while (!reader.endOfStream()) {
             int blockType = reader.readInt();
-            parseLengthOfBlock(reader);
+            int blockLength = lengthOfBlock(reader) - META_BYTES_PER_BLOCK_COUNT;
+            // Mark end of block
+            reader.mark(blockLength + reader.position());
             blocks.add(blockByBlockType(reader, blockType));
-            parseLengthOfBlock(reader);
+            skipLengthOfBlock(reader);
         }
         return blocks;
     }
 
-    private void parseLengthOfBlock(Reader reader) {
-        // Skip the length of block
-        reader.skip(4);
+
+    private void skipLengthOfBlock(Reader reader) {
+        lengthOfBlock(reader);
+    }
+
+    private int lengthOfBlock(Reader reader) {
+        return reader.readInt();
     }
 
     private Block blockByBlockType(Reader reader, int blockType) {
